578c73cabf8b3f29bf315e1f0ab73054412d197d
Ra1NuX <ra1nux.01@gmail.com>
2022-03-10 01:00:54 +0100

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-calculator';
+  title = 'Calculadora';
+  mostrar = false;
+  show = 'Mostrar'; 
+  handlerChange(){
+    this.mostrar = !this.mostrar;
+    this.mostrar ? this.show = 'Ocultar' : this.show = 'Mostrar';
+  }
+  fNum:number | null = null;
+  sNum:number | null = null;
+  result:number | string | null = null;
+  historial:Array<String> = [];
+  sumar() {
+    if(this.fNum == null || this.sNum == null) {this.result = 'Error'; return;}
+    this.result = this.fNum + this.sNum;
+    this.setHistorial('+');
+  }
+  restar() {
+    if(this.fNum == null || this.sNum == null) {this.result = 'Error'; return;}
+    this.result = this.fNum - this.sNum;
+    this.setHistorial('-');
+  }
+  multiplicar() {
+    if(this.fNum == null || this.sNum == null) {this.result = 'Error'; return;}
+    this.result = this.fNum * this.sNum;
+    this.setHistorial('*');
+  }
+  dividir() {
+    if(this.fNum == null || this.sNum == null) {this.result = 'Error'; return;}
+    this.result = this.fNum / this.sNum;
+    this.setHistorial('/');
+  }
+  pasarABinario() {
+    if(this.fNum == null || this.sNum == null) {this.result = 'Error'; return;} 
+    this.result = this.fNum.toString(2);
+    this.sNum = null;
+    this.setHistorial('Binario');
+  }
+  clean() {
+    this.fNum = null;
+    this.sNum = null;
+    this.result = null;
+    this.historial = [];
+  }
+  setHistorial(metodo:string) {
+    if(metodo == "Binario") {
+      this.historial.push(
+        `${this.fNum} en binario resulta ${this.result}`	
+      );}
+      else{
+    this.historial.push(
+      `${this.fNum} ${metodo} ${this.sNum} = ${this.result}`	
+    );
+  }
+  }
 }
